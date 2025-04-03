26c72f9e758782afeea925ea7aa4dcc4501d1844
Pooja Patel <158751282+PoojasPatel013@users.noreply.github.com>
2025-04-03 09:57:11 +0530

    fi..

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
       {/* Main Content */}
       <Header />
       <main>
-        {/* <Hero />
+        <Hero />
         <About />
         <Skills />
         <Projects />
@@ -34,15 +34,8 @@ function App() {
         <Certifications />
         <EducationAchievements/>
         {/* <ServicesSection/> */}
-        {/* <Memberships />
-        <Contact /> */}
-        <main>
-  <h1 style={{ color: "red", textAlign: "center" }}>Main Content Test</h1>
-  <Hero />
-  <About />
-  <Skills />
-</main>
-
+        <Memberships />
+        <Contact />
       </main>
       <Footer />
     </div>
